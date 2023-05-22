0de739fabeac37b4efb2beae1b841410211d316a
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2023-05-22 18:23:18 +0200

    Mise à jour du service et du controller

--- toh-back/src/main/java/topmodel/toh/api/server/heroes/HeroesControllerImpl.java
+++ toh-back/src/main/java/topmodel/toh/api/server/heroes/HeroesControllerImpl.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import topmodel.toh.dtos.heroes.HeroCreationDto;
 import topmodel.toh.dtos.heroes.HeroDto;
+import topmodel.toh.dtos.heroes.HeroUpdateDto;
 import topmodel.toh.services.heroes.HeroesService;
 
 @RestController
@@ -20,4 +23,25 @@ public class HeroesControllerImpl implements HeroesController {
 	public List<HeroDto> searchHeroes(String term) {
 		return heroesService.searchHeroes(term);
 	}
+
+	@Override
+	public HeroDto createHero(@Valid HeroCreationDto heroCreate) {
+		return heroesService.createHero(heroCreate);
+	}
+
+	@Override
+	public HeroDto getHero(Long herId) {
+		return heroesService.getHero(herId);
+	}
+
+	@Override
+	public HeroDto updateHero(Long herId, HeroUpdateDto heroUpdate) {
+		return heroesService.updateHero(herId, heroUpdate);
+	}
+
+	@Override
+	public void deleteHero(Long herId) {
+		heroesService.deleteHero(herId);
+	}
+
 }
--- toh-back/src/main/java/topmodel/toh/services/heroes/HeroesService.java
+++ toh-back/src/main/java/topmodel/toh/services/heroes/HeroesService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
 import topmodel.toh.daos.heroes.HeroDAO;
+import topmodel.toh.dtos.heroes.HeroCreationDto;
 import topmodel.toh.dtos.heroes.HeroDto;
+import topmodel.toh.dtos.heroes.HeroUpdateDto;
 
 @Service
 public class HeroesService {
@@ -26,5 +29,48 @@ public class HeroesService {
 
 		term = "%" + term + "%";
 		return heroDAO.findByNameIgnoreCaseLike(term).stream().map(HeroDto::new).toList();
+	}
+
+	/**
+	 * Retourne le détail d'un héro
+	 * 
+	 * @param herId
+	 * @return
+	 */
+	public HeroDto getHero(Long herId) {
+		return new HeroDto(heroDAO.findById(herId).orElseThrow());
+	}
+
+	/**
+	 * @param herId      Id du héro à modifier
+	 * @param heroUpdate champs à modifier
+	 * @return objet de consultation du héro
+	 */
+	public HeroDto updateHero(Long herId, HeroUpdateDto heroUpdate) {
+		var hero = heroDAO.findById(herId).orElseThrow();
+		heroUpdate.toHero(hero);
+		var savedHero = heroDAO.save(hero);
+		return new HeroDto(savedHero);
+	}
+
+	/**
+	 * Création d'un héro
+	 * 
+	 * @param heroCreate le héro à créer
+	 * @return le héro créé
+	 */
+	public HeroDto createHero(@Valid HeroCreationDto heroCreate) {
+		var savedHero = heroDAO.save(heroCreate.toHero(null));
+		return new HeroDto(savedHero);
+	}
+
+	/**
+	 * 
+	 * Suppression d'un héro par son id.
+	 * 
+	 * @param herId identifiant du héro à supprimer
+	 */
+	public void deleteHero(Long herId) {
+		heroDAO.deleteById(herId);
 	}
 }
