ddd32c13a948dcfbb30cafb5e567f760a9a9695e
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2023-05-22 18:14:34 +0200

    Génération du DAO

---
+++ toh-back/src/main/javagen/topmodel/toh/daos/heroes/HeroDAO.java
@@ -0,0 +1,14 @@
+////
+//// ATTENTION CE FICHIER EST GENERE AUTOMATIQUEMENT !
+////
+
+package topmodel.toh.daos.heroes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import topmodel.toh.entities.heroes.Hero;
+
+
+public interface HeroDAO extends JpaRepository<Hero, Long> {
+
+}
