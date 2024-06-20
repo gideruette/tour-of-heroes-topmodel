9eae193d9ae454592a68d9bbaa9086950c0c90cc
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2024-06-20 16:12:36 +0200

    Exemple d'utilisation de signal

--- toh-front/src/app/dashboard/dashboard.component.ts
+++ toh-front/src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,28 @@
 import { HeroService } from '@/appgenerated/api/heroes/hero';
 import { HeroDto } from '@/appgenerated/model/heroes/hero-dto';
-import { Component, OnInit } from '@angular/core';
-import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css'],
-    standalone: true,
-    imports: [RouterLink, HeroSearchComponent]
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  standalone: true,
+  imports: [RouterLink, HeroSearchComponent],
 })
 export class DashboardComponent implements OnInit {
-  heroes: HeroDto[] = [];
+  heroes = signal<HeroDto[]>([]); // Remplacer le signal par un array directement pour illustrer le comportement OnPush
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => this.heroes.set(heroes.slice(1, 5)));
   }
 }
